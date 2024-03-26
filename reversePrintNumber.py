8f794846d010e863f7d346bbf0181a6ae81ffc0b
Param-PS <109647522+Param-PS@users.noreply.github.com>
2024-03-26 12:20:09 +0530

    Add files via upload

---
+++ reversePrintNumber.py
@@ -0,0 +1,8 @@
+# This program to print numbers from 10 to 1
+
+for number in range(10, 0, -1):
+	print(number) #This will print each number in new line
+
+for number in range(10, 0, -1):
+	print(number, end=' ') #This will print each number in same line with space
+
